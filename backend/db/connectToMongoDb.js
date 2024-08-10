cdd68c7807343e716e06cb9ae0d20298f097d753
gurudaspr <gurudaspranavam007@gmail.com>
2024-08-10 21:45:14 +0530

    updated connectdb

--- backend/db/connectToMongoDb.js
+++ backend/db/connectToMongoDb.js
@@ -1,9 +1,15 @@
-import mongoose  from 'mongoose'
+import mongoose from 'mongoose'
 
-const connectTomongoDb = ()=>{mongoose.connect(process.env.MONGO_DB_URL)
-    .then(()=>{
-    console.log('DB connected successfully');
-})
-.catch(err => console.log('Error connecting to DB',err))
+const connectTomongoDb = () => {
+    mongoose.connect(process.env.MONGO_DB_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,
+        })
+        .then(() => {
+            console.log('DB connected successfully');
+        })
+        .catch(err => console.log('Error connecting to DB', err))
 }
 export default connectTomongoDb;
