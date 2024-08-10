f268bf2cc79af6e3846b070d458ac47f9a995794
gurudaspr <gurudaspranavam007@gmail.com>
2024-08-10 21:50:21 +0530

    updated connectdb

--- backend/db/connectToMongoDb.js
+++ backend/db/connectToMongoDb.js
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose'
 
 const connectTomongoDb = () => {
-    mongoose.connect(process.env.MONGO_DB_URL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000,
-        })
-        .then(() => {
-            console.log('DB connected successfully');
-        })
-        .catch(err => console.log('Error connecting to DB', err))
+    try {
+        mongoose.connect(process.env.MONGO_DB_URL)
+        console.log('MongoDB connected')
+    }
+    catch (err) {
+        console.log(err)
+    }
+
 }
 export default connectTomongoDb;
