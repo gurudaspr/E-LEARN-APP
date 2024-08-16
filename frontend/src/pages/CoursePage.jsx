cdfc36105aef297c7e9bdaa59164b8cc999a6c45
gurudaspr <gurudaspranavam007@gmail.com>
2024-08-16 19:27:48 +0530

    edit course page

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -62,7 +62,7 @@ export default function CoursePage() {
   };
 
   return (
-    <div className="h-min-screen bg-slate-200 py-10 min-h-screen">
+    <div className="h-min-screen h-full bg-slate-200 py-10 ">
       <h1 className='text-center text-4xl font-mono md:text-7xl pb-10'>Our Courses</h1>
       <div className="container mx-auto">
         {loading ? (
